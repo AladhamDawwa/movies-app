5c36c5713ac330f056a76f765880aba4d560f906
AladhamDawwa <aladhamdawwa@gmail.com>
2025-02-01 23:54:47 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
       <head>
         <meta property="og:title" content="Movies-app"></meta>
         <meta property="og:description" content="Adhoom's movie app."></meta>
-        <meta property="og:image" content="../../public/hero-bg.jpg"></meta>
+        <meta
+          property="og:image"
+          content="https://media.istockphoto.com/id/1295114854/photo/empty-red-armchairs-of-a-theater-ready-for-a-show.jpg?s=1024x1024&w=is&k=20&c=cSI-8stXlD8mieP_sD7jCDdWwrSXEV1w7TbTZo2TSRs="
+        ></meta>
         {/* <meta property="og:url" content=""></meta> */}
         <meta property="og:type" content="website"></meta>
       </head>
